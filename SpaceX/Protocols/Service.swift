7ee9c5f014bfe8c8baf029de0785193d62496508
Caleb Danielsen <calebclientmanifold@gmail.com>
2023-04-02 15:12:11 +0200

    add fetch and check reponse

---
+++ SpaceX/Protocols/Service.swift
@@ -0,0 +1,10 @@
+//
+//  Service.swift
+//  SpaceX
+//
+//  Created by Caleb Danielsen on 02.04.2023.
+//
+
+protocol Service {
+    func fetch()
+}
---
+++ SpaceX/Services/HTTP.swift
@@ -0,0 +1,32 @@
+//
+//  HTTP.swift
+//  SpaceX
+//
+//  Created by Caleb Danielsen on 02.04.2023.
+//
+
+import Foundation
+
+final class HTTP {
+    private init() {}
+    
+    /// Sending the HTTP request to the server and receives HTTP response with valid data, if the status code is valid.
+    static func requestResponse(_ urlString: String, statusCode: Int) async -> Data? {
+        do {
+            guard let url = URL(string: urlString) else { throw ParseError.parseStringToURLFailed }
+            let (data, response) = try await URLSession.shared.data(from: url)
+            try checkStatusCode(response: response, statusCode: statusCode)
+            return data
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+    
+    /// Checking the desired status-code
+    private static func checkStatusCode(response: URLResponse, statusCode: Int) throws {
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == statusCode
+        else { throw WebserviceError.badResponse }
+    }
+}
