90626e632228f60eadebf15e3a0cb701d8e2c8cb
Caleb Danielsen <calebclientmanifold@gmail.com>
2023-04-07 22:14:44 +0200

    add view mode buffer

--- SpaceX/Controllers/LaunchScreenController.swift
+++ SpaceX/Controllers/LaunchScreenController.swift
@@ -10,6 +10,7 @@ import SwiftUI
 /// Reponsible for showing all past SpaceX launches
 final class LaunchScreenTableViewController: UITableViewController {
     private var viewModels: [ViewModel] = []
+    private var viewModelsBuffer: [ViewModel] = []
     private var webManager = ManagerFactory.create() as! WebManager
     
     lazy var launchFilterBarButtonItem: UIBarButtonItem = {
@@ -36,7 +37,7 @@ final class LaunchScreenTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Constant.LaunchScreen.Cell.identifier, for: indexPath)
         cell.accessoryType = .disclosureIndicator
-        cell.contentConfiguration = UIHostingConfiguration { LaunchCellView(launchViewModel: viewModels[indexPath.row] as! LaunchViewModel) }
+        cell.contentConfiguration = UIHostingConfiguration { LaunchCellView(launchViewModel: viewModelsBuffer[indexPath.row] as! LaunchViewModel) }
         return cell
     }
     
@@ -51,6 +52,7 @@ extension LaunchScreenTableViewController: WebManagerDelegate {
     func update(viewModels: [ViewModel]) {
         Task {
             self.viewModels = viewModels
+            self.viewModelsBuffer = viewModels
             tableView.reloadData()
         }
     }
@@ -64,27 +66,27 @@ extension LaunchScreenTableViewController {
         
         alertController.addAction(UIAlertAction(title: Constant.LaunchScreen.NavigationItemButton.newest, style: .default) { [weak self] _ in
             Context.setStrategy(strategy: NewestDateStrategy())
-            self?.viewModels = Context.executeStrategy(viewModels: self?.viewModels as! [LaunchViewModel])
+            self?.viewModelsBuffer = Context.executeStrategy(viewModels: self?.viewModels as! [LaunchViewModel])
             self?.tableView.reloadData()
         })
         alertController.addAction(UIAlertAction(title: Constant.LaunchScreen.NavigationItemButton.oldest, style: .default) { [weak self] _ in
             Context.setStrategy(strategy: OldestDateStrategy())
-            self?.viewModels = Context.executeStrategy(viewModels: self?.viewModels as! [LaunchViewModel])
+            self?.viewModelsBuffer = Context.executeStrategy(viewModels: self?.viewModels as! [LaunchViewModel])
             self?.tableView.reloadData()
         })
         alertController.addAction(UIAlertAction(title: Constant.LaunchScreen.NavigationItemButton.success, style: .default) { [weak self] _ in
             Context.setStrategy(strategy: SuccessStrategy())
-            self?.viewModels = Context.executeStrategy(viewModels: self?.viewModels as! [LaunchViewModel])
+            self?.viewModelsBuffer = Context.executeStrategy(viewModels: self?.viewModels as! [LaunchViewModel])
             self?.tableView.reloadData()
         })
         alertController.addAction(UIAlertAction(title: Constant.LaunchScreen.NavigationItemButton.failure, style: .default) { [weak self] _ in
             Context.setStrategy(strategy: FailureStrategy())
-            self?.viewModels = Context.executeStrategy(viewModels: self?.viewModels as! [LaunchViewModel])
+            self?.viewModelsBuffer = Context.executeStrategy(viewModels: self?.viewModels as! [LaunchViewModel])
             self?.tableView.reloadData()
         })
         alertController.addAction(UIAlertAction(title: Constant.LaunchScreen.NavigationItemButton.name, style: .default) { [weak self] _ in
             Context.setStrategy(strategy: TitleStrategy())
-            self?.viewModels = Context.executeStrategy(viewModels: self?.viewModels as! [LaunchViewModel])
+            self?.viewModelsBuffer = Context.executeStrategy(viewModels: self?.viewModels as! [LaunchViewModel])
             self?.tableView.reloadData()
         })
         
