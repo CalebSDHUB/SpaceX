f8668df249cc9cba9d8290d462b8d6fdd97f1178
Caleb Danielsen <calebclientmanifold@gmail.com>
2023-04-08 23:21:44 +0200

    add Filter

--- SpaceX/Strategies/Filter/Filter.swift
+++ SpaceX/Strategies/Filter/Filter.swift
@@ -5,4 +5,16 @@
 //  Created by Caleb Danielsen on 08.04.2023.
 //
 
-import Foundation
+/// Responsible for setting and executing the current strategy using the Strategy Design Pattern
+final class Filter {
+    static private var strategy: FilterStrategy!
+    private init() {}
+    
+    static func setStrategy(strategy: FilterStrategy) {
+        self.strategy = strategy
+    }
+    
+    static func executeStrategy(viewModels: [ViewModel], text: String) -> [ViewModel] {
+        strategy.execute(viewModels: viewModels, text: text)
+    }
+}
